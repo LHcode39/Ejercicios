4b9d78d11a7f63a30dfe844814a8d2f9947d61b4
LHcode39 <101009336+LHcode39@users.noreply.github.com>
2022-03-19 11:58:23 -0600

    arreglos

---
+++ 11Arreglos.py
@@ -0,0 +1,13 @@
+#11. Hacer una función para que imprima un arreglo o lista de 10 elementos numéricos
+
+informacion = [ 7, 4 ,34, 17, 29, 1, 52, 98, 39, 121, 25]
+#               0   1  2  3   4   5  6    7   8   9    10
+
+
+informacion [9] = 67
+informacion.append(200)
+informacion.sort()
+
+#SALIDA
+
+print(informacion)
